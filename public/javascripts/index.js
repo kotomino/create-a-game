805a853c6af594d0838f2d3cb76a6d8080b70b9e
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-03-09 18:03:12 -0500

    setup canvas

---
+++ public/javascripts/index.js
@@ -0,0 +1,13 @@
+
+function drawBackground() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+}
+
+function setup() {
+  drawBackground();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  setup();
+})
